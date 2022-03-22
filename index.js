a9d6eb6924daf2291b5939a0001d482272cc6dc1
Jerry <Jvictor940@gmail.com>
2022-03-22 16:28:02 -0400

    commit tree 63f7d8fd1a8eab55c3a7a95fcc7b6d40e1c8895b

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Write your code here!
+document.getElementById('main').remove()
+
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.innerHTML = 'Jerry is the champion';
+document.body.append(newHeader);
+
+
+//document.grabElementByTagName('h1').append('Jerry is the champion')
+
+// const header = document.querySelector('h1#victory')
+// console.log(header)
+//newHeader.append('Jerry is the champion');
+
